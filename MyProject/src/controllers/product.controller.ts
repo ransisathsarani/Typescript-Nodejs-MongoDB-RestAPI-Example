5f2969b27fd20f3f08c00072a283126aa4fe47b8
sathsaranir1996 <38213964+sathsaranir1996@users.noreply.github.com>
2021-06-10 17:11:42 +0530

    minor fixes

--- MyProject/src/controllers/product.controller.ts
+++ MyProject/src/controllers/product.controller.ts
@@ -3,7 +3,6 @@ import Product from "../models/Product";
 import path from "path";
 import fs from "fs-extra";
 
-
 // create new product
 export async function createProduct(
   req: Request,
@@ -19,30 +18,44 @@ export async function createProduct(
     imagePath: req.file.path,
   };
 
-  const product = new Product(newProduct);
-  await product.save();
+  try {
+    const product = new Product(newProduct);
+    await product.save();
 
-  return res.json({
-    code: 200,
-    message: "Product Saved Successfully",
-    data: product
-  });
+    return res.status(200).json({
+      code: 200,
+      message: "Product Saved Successfully",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Server Error",
+      data: null,
+    });
+  }
 }
-
 
 // Get product list
 export async function productList(
   req: Request,
   res: Response
 ): Promise<Response> {
-  const product = await Product.find();
-  return res.json({
-    code: 200,
-    message: "Success",
-    data: product
-  });
+  try {
+    const product = await Product.find();
+    return res.status(200).json({
+      code: 200,
+      message: "Success",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Server Error",
+      data: null,
+    });
+  }
 }
-
 
 //Get product by ID
 export async function getProductById(
@@ -50,14 +63,22 @@ export async function getProductById(
   res: Response
 ): Promise<Response> {
   const { id } = req.params;
-  const product = await Product.findById(id);
-  return res.json({
-    code: 200,
-    message: "Success",
-    data: product
-  });
-}
 
+  try {
+    const product = await Product.findById(id);
+    return res.status(200).json({
+      code: 200,
+      message: "Success",
+      data: product,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      code: 404,
+      message: "Product Not Found",
+      data: null,
+    });
+  }
+}
 
 //   Delete product by Id
 export async function deleteProduct(
@@ -65,34 +86,54 @@ export async function deleteProduct(
   res: Response
 ): Promise<Response> {
   const { id } = req.params;
-  const product = await Product.findByIdAndRemove(id);
-//   if(product){
-//       await fs.unlink(path.resolve(product.imagePath));
-//   }
-  return res.json({
-    code: 200,
-    message: "Successfully Deleted"
-  });
-}
 
+  try {
+    const product = await Product.findByIdAndRemove(id);
+    //   if(product){
+    //       await fs.unlink(path.resolve(product.imagePath));
+    //   }
+    return res.status(200).json({
+      code: 200,
+      message: "Successfully Deleted",
+    });
+  } catch (error) {
+    return res.status(404).json({
+      code: 404,
+      message: "Product Not Found",
+      data: null,
+    });
+  }
+}
 
 // Update Product by Id
 export async function updateProduct(
-    req: Request,
-    res: Response
-  ): Promise<Response> {
-    const { id } = req.params;
-    const { name, code, description } = req.body;
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const { id } = req.params;
+  const { name, code, description } = req.body;
 
-    const updatedProduct = await Product.findByIdAndUpdate(id, {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
         name,
         code,
-        description
-    }, {new: true});
-   
-    return res.json({
+        description,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({
       code: 200,
       message: "Product Updated Successfully",
-      data: updatedProduct
+      data: updatedProduct,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      code: 404,
+      message: "Product Not Found",
+      data: null,
     });
   }
+}
